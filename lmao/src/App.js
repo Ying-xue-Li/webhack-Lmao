aa3a7223e1030a40037d49765c19f7079da174fb
yingxue0323 <yingxue0323@gmail.com>
2024-05-05 10:44:46 +1000

    Added a help ai chat box

--- lmao/src/App.js
+++ lmao/src/App.js
@@ -1,13 +1,32 @@
 import AudioPlayer from './components/audioPlayer';
 import './App.css';
 import { Timer } from './components/Timer';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <script>
+          {`
+          window.embeddedChatbotConfig = {
+            chatbotId: "c-_ZW2fcqEn37DlrXw6Np",
+            domain: "www.chatbase.co"
+          };
+          `}
+        </script>
+        <script
+          src="https://www.chatbase.co/embed.min.js"
+          chatbotId="c-_ZW2fcqEn37DlrXw6Np"
+          domain="www.chatbase.co"
+          defer
+        />
+      </Helmet>
       <h1>purring player</h1>
       <AudioPlayer />
       <Timer />
+      
+
     </div>
   );
 }
